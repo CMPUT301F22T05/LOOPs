43342d892edd87fcf762861aad7b9847ba109d24
Hengshuo <shenhengshuo@gmail.com>
2022-10-26 20:00:24 -0600

    update minSDK to 24 in build.gradle(:app) and implement sort(...) in IngredientCollection.java and needed comparators

--- LOOPs/app/src/main/java/com/example/loops/IngredientCollection.java
+++ LOOPs/app/src/main/java/com/example/loops/IngredientCollection.java
@@ -1,6 +1,8 @@
 package com.example.loops;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class IngredientCollection {
     private ArrayList<Ingredient> ingredients;
@@ -22,6 +24,46 @@ public class IngredientCollection {
     }
 
     public void sort(IngredientSortOption option) {
+        if (option.equals(IngredientSortOption.BY_DESCRIPTION_ASCENDING)) {
+            Collections.sort(ingredients, new DescriptionAscendingComparator());
+        }
+        else if (option.equals(IngredientSortOption.BY_BEST_BEFORE_DATE_ASCENDING)) {
+            ingredients.sort(new BestBeforeDateAscendingComparator());
+        }
+        else if (option.equals(IngredientSortOption.BY_CATEGORY_ASCENDING)) {
+            ingredients.sort(new CategoryAscendingComparator());
+        }
+        else if (option.equals(IngredientSortOption.BY_LOCATION_ASCENDING)) {
+            Collections.sort(ingredients, new LocationAscendingComparator());
+        }
+    }
 
+    class DescriptionAscendingComparator implements Comparator<Ingredient> {
+
+        @Override
+        public int compare(Ingredient o1, Ingredient o2) {
+            return o1.getDescription().compareTo(o2.getDescription());
+        }
+    }
+
+    class BestBeforeDateAscendingComparator implements Comparator<Ingredient> {
+        @Override
+        public int compare(Ingredient o1, Ingredient o2) {
+            return o1.getBestBeforeDate().compareTo(o2.getBestBeforeDate());
+        }
+    }
+
+    class CategoryAscendingComparator implements Comparator<Ingredient> {
+        @Override
+        public int compare(Ingredient o1, Ingredient o2) {
+            return o1.getCategory().compareTo(o2.getCategory());
+        }
+    }
+
+    class LocationAscendingComparator implements Comparator<Ingredient> {
+        @Override
+        public int compare(Ingredient o1, Ingredient o2) {
+            return o1.getStoreLocation().compareTo(o2.getStoreLocation());
+        }
     }
 }
