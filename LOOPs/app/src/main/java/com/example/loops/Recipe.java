e07cec4b541077b564a67cb31a551934c2db71d7
JadeMJ <moorejac@ualberta.ca>
2022-10-27 15:35:45 -0600

    Added recipe test class (shell)

--- LOOPs/app/src/main/java/com/example/loops/Recipe.java
+++ LOOPs/app/src/main/java/com/example/loops/Recipe.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 
 import java.time.LocalTime;
 
+/**
+ *  model class of a Recipe
+ */
 public class Recipe {
     private String title;
     private LocalTime prepTime;
@@ -13,6 +16,55 @@ public class Recipe {
     private Bitmap photo;
     private Uri picUri;
     private IngredientCollection ingredients;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalTime getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(LocalTime prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public int getNumServing() {
+        return numServing;
+    }
+
+    public void setNumServing(int numServing) {
+        this.numServing = numServing;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+
+    public Uri getPicUri() {
+        return picUri;
+    }
+
+    public void setPicUri(Uri picUri) {
+        this.picUri = picUri;
+    }
 
 
 }
---
+++ LOOPs/app/src/test/java/com/example/loops/RecipeTest.java
@@ -0,0 +1,49 @@
+package com.example.loops;
+
+import junit.framework.TestCase;
+
+public class RecipeTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetTitle() {
+    }
+
+    public void testSetTitle() {
+    }
+
+    public void testGetPrepTime() {
+    }
+
+    public void testSetPrepTime() {
+    }
+
+    public void testGetNumServing() {
+    }
+
+    public void testSetNumServing() {
+    }
+
+    public void testGetCategory() {
+    }
+
+    public void testSetCategory() {
+    }
+
+    public void testGetPhoto() {
+    }
+
+    public void testSetPhoto() {
+    }
+
+    public void testGetPicUri() {
+    }
+
+    public void testSetPicUri() {
+    }
+}
