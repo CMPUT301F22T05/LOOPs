70cc9c6e12ffb38c027cfac2eca8bc5ae181fdc1
Scott <shur1@ualberta.ca>
2022-10-28 01:19:15 -0600

    EditIngredientFragment partially implemented - TODO: implement method initializeFormWithIngredientAttributes - TODO: implement sendResult

--- LOOPs/app/src/main/java/com/example/loops/EditIngredientFragment.java
+++ LOOPs/app/src/main/java/com/example/loops/EditIngredientFragment.java
@@ -4,23 +4,18 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * An ingredient form for editing ingredients. Supply the ingredient to edit through action args
  */
 public class EditIngredientFragment extends IngredientFormFragment {
-    public static final String INGREDIENT_RESULT_KEY = "EDIT_INGREDIENT_FORM_RESULT_KEY";
 
     public EditIngredientFragment() { }
 
     /**
-     * Set up event listeners
+     * Set up event listeners and sets button text
      * @param formView
      * @param savedInstanceState
      */
@@ -30,22 +25,14 @@ public class EditIngredientFragment extends IngredientFormFragment {
         submitButton.setText("Edit");
     }
 
+    // TODO: Implement this
     public void initializeFormWithIngredientAttributes() {
+
         return;
     }
 
-    void sendResult(String desc) {
-//        This could be useful.
-//        https://developer.android.com/guide/fragments/communicate#fragment-result
-        Bundle ingredientBundle = new Bundle();
-//        FIXME: can't submit ingredient right now... Hardcoded to description for now...
-        ingredientBundle.putString(INGREDIENT_RESULT_KEY, desc);
-        FragmentManager fragmentManager = getParentFragmentManager();
-        fragmentManager.setFragmentResult(INGREDIENT_RESULT_KEY, ingredientBundle);
-        closeFragment();
-    }
-
-    private void closeFragment() {
-        getActivity().getFragmentManager().popBackStack();
+    // TODO: Implement this
+    void sendResult(Ingredient submittedIngredient) {
+        return;
     }
 }
