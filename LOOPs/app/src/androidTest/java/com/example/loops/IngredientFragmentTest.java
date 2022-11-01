a0375737eecc4e0f08c88d4114de9510880cb75e
CoasterJX <jianxi1@ualberta.ca>
2022-10-31 18:33:22 -0600

    IngredientFragment: test file added

---
+++ LOOPs/app/src/androidTest/java/com/example/loops/IngredientFragmentTest.java
@@ -0,0 +1,11 @@
+package com.example.loops;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class IngredientFragmentTest {
+}
