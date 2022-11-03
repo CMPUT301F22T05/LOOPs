d1e6f37773f4f3d8f80634bb152cf7a6b774f14a
Charles Perez <perezcharles1112@gmail.com>
2022-11-03 14:43:26 -0600

    This is an interface to implement on click

---
+++ LOOPs/app/src/main/java/com/example/loops/RecyclerViewOnClickInterface.java
@@ -0,0 +1,8 @@
+package com.example.loops;
+
+/**
+ * This is an interface to add an on click listerner on a recycler void
+ */
+public interface RecyclerViewOnClickInterface {
+    public void OnItemClick(int position);
+}
