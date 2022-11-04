45103c08302d9f2ea7342cd5493a5a63728178e7
PAULbigBA <paulbigba@outlook.com>
2022-11-04 02:56:51 -0600

    add recipeFormFranment

--- LOOPs/app/src/main/java/com/example/loops/Recipe.java
+++ LOOPs/app/src/main/java/com/example/loops/Recipe.java
@@ -31,6 +31,7 @@ public class Recipe implements Serializable {
             this.numServing = numServ;
             this.category = category;
             this.comments = comment;
+            this.ingredients = new IngredientCollection();
     }
 
     public String getComments() {
