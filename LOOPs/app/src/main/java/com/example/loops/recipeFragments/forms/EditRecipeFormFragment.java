455d2497f6872e02c3ce6be7af929422e2c3c49c
JadeMJ <moorejac@ualberta.ca>
2022-11-15 18:36:35 -0700

    Update EditRecipeFormFragment.java

--- LOOPs/app/src/main/java/com/example/loops/recipeFragments/forms/EditRecipeFormFragment.java
+++ LOOPs/app/src/main/java/com/example/loops/recipeFragments/forms/EditRecipeFormFragment.java
@@ -17,7 +17,7 @@ import com.example.loops.models.Ingredient;
 import com.example.loops.models.Recipe;
 
 /**
- * An ingredient form for editing recipes. Supply the recipe to edit through action args
+ * A recipe form for editing recipes. Supply the recipe to edit through action args
  */
 public class EditRecipeFormFragment extends RecipeFormFragment {
 
