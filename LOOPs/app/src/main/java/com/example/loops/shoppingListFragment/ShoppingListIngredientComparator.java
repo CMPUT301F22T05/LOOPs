e44bdc036774d212bbadae513f7142cbc71227ff
CoasterJX <jianxi1@ualberta.ca>
2022-11-19 18:13:25 -0700

    shopping list generator started

---
+++ LOOPs/app/src/main/java/com/example/loops/shoppingListFragment/ShoppingListIngredientComparator.java
@@ -0,0 +1,11 @@
+package com.example.loops.shoppingListFragment;
+
+import com.example.loops.models.Ingredient;
+
+public class ShoppingListIngredientComparator {
+
+    public static int isTheSameIngredient(Ingredient ingredient1, Ingredient ingredient2) {
+        return;
+    }
+
+}
---
+++ LOOPs/app/src/main/java/com/example/loops/shoppingListFragment/ShoppingListInitializer.java
@@ -0,0 +1,8 @@
+package com.example.loops.shoppingListFragment;
+
+import com.example.loops.modelCollections.IngredientCollection;
+
+public class ShoppingListInitializer {
+
+    public static IngredientCollection getShoppingList() {}
+}
