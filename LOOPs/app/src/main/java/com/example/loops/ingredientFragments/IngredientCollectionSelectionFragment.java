632ff6080db1259155a60b3bb9e8df8665375dec
Scott <shur1@ualberta.ca>
2022-11-20 20:13:17 -0700

    Implemented prompt for user to input ingredient amount and unit when selecting an ingredient

--- LOOPs/app/src/main/java/com/example/loops/ingredientFragments/IngredientCollectionSelectionFragment.java
+++ LOOPs/app/src/main/java/com/example/loops/ingredientFragments/IngredientCollectionSelectionFragment.java
@@ -17,7 +17,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.loops.adapters.IngredientSelectionViewAdapter;
 import com.example.loops.adapters.IngredientStorageViewAdapter;
@@ -26,6 +28,9 @@ import com.example.loops.factory.IngredientCollectionFactory.CollectionType;
 import com.example.loops.models.Ingredient;
 import com.example.loops.R;
 import com.example.loops.recipeFragments.forms.AddRecipeFormFragmentDirections;
+import com.example.loops.validators.IngredientValidator;
+
+import java.util.ArrayList;
 
 /**
  * Ingredient collection fragment for selecting an ingredient
@@ -102,6 +107,9 @@ public class IngredientCollectionSelectionFragment extends IngredientCollectionF
         getArguments().clear();
     }
 
+    /**
+     * Defines the behavior when save button is clicked
+     */
     private void setSaveButtonListener() {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,38 +136,117 @@ public class IngredientCollectionSelectionFragment extends IngredientCollectionF
         }
         // If not, add it to selection
         else {
-            // Prompt for quantity
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            // TODO : Create UI for alert dialog to input amount and unit
-            AlertDialog setIngredientQuantityPrompt = builder
-                    .setTitle( "Select Quantity" )
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // On quantity set, select it
-                            selectedIngredient.setAmount(6.9);
-                            ((IngredientSelectionViewAdapter) collectionViewAdapter).selectItem(position);
-                            chosenIngredients.addIngredient(selectedIngredient);
-                        }
-                    })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // On cancel, do nothing
-                            return;
-                        }
-                    })
-                    .create();
-            setIngredientQuantityPrompt.show();
+            openIngredientQuantityPrompt(selectedIngredient, position);
         }
+    }
+
+    /**
+     * Opens the dialog to prompt for user to input quantity of selected ingredient
+     * It also selects the ingredients in the adapter
+     * @param selectedIngredient
+     * @param position position of the selected ingredient in the adapter
+     */
+    private void openIngredientQuantityPrompt(Ingredient selectedIngredient, int position) {
+        /**
+         * https://stackoverflow.com/questions/2620444/how-to-prevent-a-dialog-from-closing-when-a-button-is-clicked
+         * Author : Tom Bollwitt
+         * Date Accessed : 2022-11-20
+         */
+        // Create view for the prompt
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View ingredientQuantityPromptView = inflater.inflate(R.layout.dialog_ingredient_quantity_prompt, null);
+        EditText amountInput = ingredientQuantityPromptView.findViewById(R.id.dialog_amount_input);
+        Spinner unitInput = ingredientQuantityPromptView.findViewById(R.id.dialog_unit_spinner);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog setIngredientQuantityPrompt = builder
+                .setTitle( "Select Quantity for " + selectedIngredient.getDescription() )
+                .setView(ingredientQuantityPromptView)
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;     // Do nothing on cancel
+                    }
+                })
+                .create();
+        // We implement the positive button on click listener separately to prevent the
+        // dialog closing on button click automatically. We want to control when it closes.
+        setIngredientQuantityPrompt.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+
+                Button button = setIngredientQuantityPrompt.getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Get user input
+                        double amount = getAmountFromInput(amountInput);
+                        String unit = unitInput.getSelectedItem().toString();
+                        // Validate user input
+                        IngredientValidator validator = new IngredientValidator();
+                        boolean valid = validator.checkAmount(amount, IngredientValidator.INGREDIENT_TYPE.STORED);
+                        valid = validator.checkUnit(unit, IngredientValidator.INGREDIENT_TYPE.STORED) && valid;
+
+                        if (valid) {
+                            selectedIngredient.setAmount(amount);
+                            selectedIngredient.setUnit(unit);
+                            chosenIngredients.addIngredient(selectedIngredient);
+                            ((IngredientSelectionViewAdapter) collectionViewAdapter).selectItem(position);
+                            setIngredientQuantityPrompt.dismiss();
+                        }
+                        else {
+                            ArrayList<String> errorMessages = new ArrayList<>();
+                            for (int errorStringID : validator.getErrorStringIds()) {
+                                errorMessages.add( getString(errorStringID) );
+                            }
+                            displayErrorMessages(errorMessages);
+                        }
+                    }
+                });
+            }
+        });
+        setIngredientQuantityPrompt.show();
+    }
+
+    /**
+     * Displays error messages in a dialog
+     * @param errorMessages the strings of error messages to show
+     */
+    private void displayErrorMessages(ArrayList<String> errorMessages) {
+        // FIXME: maybe this should be a custom view since it has been reused from ingredient form
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog errorMessageDisplay = builder
+                .setTitle("Please fill out the form properly")
+                .setMessage( String.join("\n", errorMessages) )
+                .create();
+        errorMessageDisplay.show();
     }
 
     /**
      * Sends the ingredient to the caller fragment
      */
-    void sendIngredientsToCallerFragment() {
+    private void sendIngredientsToCallerFragment() {
         Navigation.findNavController(getView()).getPreviousBackStackEntry().getSavedStateHandle().set(
                 RESULT_KEY,
                 chosenIngredients
         );
         Navigation.findNavController(getView()).popBackStack();
+    }
+
+    /**
+     * Parses the user input in the edit text for amount
+     * @param amountInput edit text for amount
+     * @return double. If invalid input, returns NaN
+     */
+    private double getAmountFromInput(EditText amountInput) {
+        double amount = Double.NaN;
+        try {
+            amount = Double.parseDouble( amountInput.getText().toString() );
+        }
+        catch (NumberFormatException e) {
+            amount = Double.NaN;
+        }
+        return amount;
     }
 }
