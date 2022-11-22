2e2c77e136d3a14ca5b6c878dfbfd3c4115a0674
Charles Perez <perezcharles1112@gmail.com>
2022-11-22 04:02:35 -0700

    Added test for invalid recipes, valid recipes, testing the camera

--- LOOPs/app/src/androidTest/java/com/example/loops/AddRecipeFormFragmentTest.java
+++ LOOPs/app/src/androidTest/java/com/example/loops/AddRecipeFormFragmentTest.java
@@ -1,24 +1,43 @@
 package com.example.loops;
 
+import static android.os.SystemClock.sleep;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.example.loops.ImageViewHasDrawableMatcher.hasDrawable;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import com.example.loops.ingredientFragments.forms.AddIngredientFormFragment;
+import com.example.loops.modelCollections.IngredientCollection;
+import com.example.loops.models.Ingredient;
 import com.example.loops.models.Recipe;
 
-import androidx.activity.result.ActivityResultLauncher;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentFactory;
@@ -30,30 +49,50 @@ import androidx.navigation.Navigation;
 import androidx.navigation.testing.TestNavHostController;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.intent.rule.IntentsRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.example.loops.ingredientFragments.forms.AddIngredientFormFragment;
 import com.example.loops.recipeFragments.forms.AddRecipeFormFragment;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Locale;
+
 @RunWith(AndroidJUnit4.class)
 public class AddRecipeFormFragmentTest {
     private TestNavHostController navController;
     private FragmentScenario<AddRecipeFormFragment> fragmentScenario;
+    private Bundle args;
+    private IngredientCollection mockIngredientCollection;
 
     @Rule
     public IntentsRule intentsRule = new IntentsRule();
     @Before
     public void setup(){
+        args = new Bundle();
+        mockIngredientCollection = new IngredientCollection();
         navController = new TestNavHostController( ApplicationProvider.getApplicationContext() );
         navController.setViewModelStore(new ViewModelStore());
-        fragmentScenario = FragmentScenario.launchInContainer(AddRecipeFormFragment.class,null,new FragmentFactory(){
+
+
+
+
+
+    }
+    /**
+     * https://developer.android.com/guide/navigation/navigation-testing#test_navigationui_with_fragmentscenario
+     * Date Accessed : 2022-11-19
+     */
+    public void launchFragment(){
+
+        fragmentScenario = FragmentScenario.launchInContainer(AddRecipeFormFragment.class,args,new FragmentFactory(){
             @NonNull
             @Override
             public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className) {
@@ -65,14 +104,44 @@ public class AddRecipeFormFragmentTest {
                         navController.setGraph(R.navigation.nav_graph);
                         navController.setCurrentDestination(R.id.addRecipeFormFragment);
                         Navigation.setViewNavController(addRecipeFormFragment.requireView(), navController);
+
+
                     }
                 });
                 return addRecipeFormFragment;
             }
+
         });
+        /*
+         * Stubs result from camera
+         */
+
+        Instrumentation.ActivityResult result = cameraResultStub();
+        Intents.intending(IntentMatchers.hasAction(MediaStore.ACTION_IMAGE_CAPTURE)).respondWith(result);
+
+    }
+
+    private void mimicIngredientCollection(){
+        Ingredient salt = new Ingredient("kosher salt", "11/14/2025","Pantry",1,"tsp","Spice");
+        mockIngredientCollection.addIngredient(salt);
+        args.putSerializable("ingredientCollection",mockIngredientCollection);
+    }
+    private Bundle getReturnBundle() {
+        return navController.getBackStack()
+                .get(navController.getBackStack().size()-1)
+                .getArguments();
+    }
 
 
-
+    private Instrumentation.ActivityResult cameraResultStub(){
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("data", BitmapFactory.decodeResource(
+                InstrumentationRegistry.getInstrumentation().getTargetContext().getResources(),
+                R.drawable.hamburger_test_image
+        ));
+        Intent resultData = new Intent();
+        resultData.putExtras(bundle);
+        return new Instrumentation.ActivityResult(Activity.RESULT_OK,resultData);
     }
 
     private String getString(int id) {
@@ -116,22 +185,18 @@ public class AddRecipeFormFragmentTest {
     private void setMinuteDuration (String minute){
         typeToEditText(R.id.recipeFormPrepTimeMinuteInput,minute);
     }
-    private Recipe getSubmittedRecipe(){
-        Recipe submittedRecipe = (Recipe) navController.getBackStack()
-                .get(navController.getBackStack().size()-1)
-                .getArguments()
-                .getSerializable("addedRecipe");
-        return submittedRecipe;
-    }
+
 
     @Test
     public void testSubmitNothing(){
+        launchFragment();
         clickSubmit();
         onView(withText("Please fill out the form properly")).check(matches(isDisplayed()));
     }
 
     @Test
     public void testTitleNotSubmitted(){
+        launchFragment();
         setComments("This is a comment");
         setCategory("Breakfast");
         setHourDuration("1");
@@ -142,6 +207,7 @@ public class AddRecipeFormFragmentTest {
     }
     @Test
     public void testNoCommentSubmitted(){
+        launchFragment();
         setTitle("Test Recipe");
         setCategory("Snack");
         setHourDuration("1");
@@ -153,6 +219,7 @@ public class AddRecipeFormFragmentTest {
 
     @Test
     public void testNoDurationInput(){
+        launchFragment();
         setCategory("Snack");
         setNumServing("4");
         setTitle("Test Recipe");
@@ -163,6 +230,7 @@ public class AddRecipeFormFragmentTest {
 
     @Test
     public void testNoNumServing(){
+        launchFragment();
         setTitle("Test Recipe");
         setCategory("Snack");
         setHourDuration("1");
@@ -174,12 +242,124 @@ public class AddRecipeFormFragmentTest {
 
     @Test
     public void testValidRecipe(){
+        launchFragment();
+        fragmentScenario.onFragment(fragment -> {
+            navController.setCurrentDestination(R.id.recipeCollectionEditorFragment);
+            navController.navigate(R.id.addRecipeFormFragment);
+        });
+        IngredientCollection testIngredients = new IngredientCollection();
+        Bitmap hamburger = BitmapFactory.decodeResource(
+                InstrumentationRegistry.getInstrumentation().getTargetContext().getResources(),
+                R.drawable.hamburger_test_image);
+        Recipe typedRecipe = new Recipe(
+                "Hamburger",
+                0,
+                45,
+                1,
+                "Lunch",
+                hamburger,
+                testIngredients,
+                "Very Delicious"
 
+        );
+        setTitle("Hamburger");
+        setCategory("Lunch");
+        setNumServing("1");
+        setComments("Very Delicious");
+        setMinuteDuration("40");
+        setHourDuration("0");
+        onView(withId(R.id.imageView)).perform(click());
+        clickSubmit();
+        sleep(1000);
+        assertEquals(navController.getCurrentDestination().getId(), R.id.recipeCollectionEditorFragment);
     }
     @Test
     public void testCamera(){
-
+        launchFragment();
+        Bitmap hamburger = BitmapFactory.decodeResource(
+                InstrumentationRegistry.getInstrumentation().getTargetContext().getResources(),
+                R.drawable.hamburger_test_image);
+        setTitle("Hamburger");
+        setCategory("Lunch");
+        setNumServing("1");
+        setComments("Very Delicious");
+        setMinuteDuration("40");
+        setHourDuration("0");
+        onView(withId(R.id.imageView)).perform(click());
+        onView(withId(R.id.imageView)).check(matches(hasDrawable()));
+        onView(withId(R.id.recipeFormCommentsInput)).check(matches(withText("Very Delicious")));
+        onView(withId(R.id.recipeFormPrepTimeMinuteInput)).check(matches(withText("40")));
+        onView(withId(R.id.recipeFormPrepTimeHourInput)).check(matches(withText("0")));
+        onView(withId(R.id.recipeFormNumServingInput)).check(matches(withText("1")));
+        onView(withId(R.id.recipeFormTitleInput)).check(matches(withText("Hamburger")));
+        onView(withId(R.id.recipeFormCategoryInput)).check(matches(withSpinnerText("Lunch")));
+    }
+    @Test
+    public void testCancelOption() {
+        launchFragment();
+        onView(withId(R.id.recipeFormAddIngredientButton)).perform(click());
+        String title = "How do you want to select an ingredient?";
+        String byNew = "By New Ingredient";
+        String fromStorage = "From Ingredient Storage";
+        String cancel = "Cancel";
+        String[] expected = {
+                title,
+                byNew,
+                fromStorage,
+                cancel
+        };
+        for (String e: expected){
+            onView(withText(equalToIgnoringCase(e))).check(matches(isDisplayed()));
+        }
+        onData(allOf(instanceOf(String.class),is(cancel))).perform(click());
+        onView(withText(cancel)).check(doesNotExist());
+    }
+    @Test
+    public void testByNewIngredientOption(){
+        launchFragment();
+        clickAddIngredient();
+        String title = "How do you want to select an ingredient?";
+        String byNew = "By New Ingredient";
+        String fromStorage = "From Ingredient Storage";
+        String cancel = "Cancel";
+        String[] expected = {
+                title,
+                byNew,
+                fromStorage,
+                cancel
+        };
+        for (String e: expected){
+            onView(withText(equalToIgnoringCase(e))).check(matches(isDisplayed()));
+        }
+        onData(allOf(instanceOf(String.class),is(byNew))).perform(click());
+        onView(withText(cancel)).check(doesNotExist());
+        assertEquals(navController.getCurrentDestination().getId(), R.id.addRecipeIngredientFormFragment);
     }
 
+    @Test
+    public void testFromIngredientStorageOption(){
+        mimicIngredientCollection();
+        launchFragment();
+        clickAddIngredient();
+        String title = "How do you want to select an ingredient?";
+        String byNew = "By New Ingredient";
+        String fromStorage = "From Ingredient Storage";
+        String cancel = "Cancel";
+        String[] expected = {
+                title,
+                byNew,
+                fromStorage,
+                cancel
+        };
+        for (String e: expected){
+            onView(withText(equalToIgnoringCase(e))).check(matches(isDisplayed()));
+        }
+        sleep(5000);
+        onData(allOf(instanceOf(String.class),is(fromStorage))).perform(click());
+        onView(withText(cancel)).check(doesNotExist());
+        assertEquals(navController.getCurrentDestination().getId(), R.id.ingredientCollectionSelectionFragment);
+        Bundle returnBundle = getReturnBundle();
+        assertEquals(returnBundle.getSerializable("ingredientsToFilter"),mockIngredientCollection);
+    }
 
 }
