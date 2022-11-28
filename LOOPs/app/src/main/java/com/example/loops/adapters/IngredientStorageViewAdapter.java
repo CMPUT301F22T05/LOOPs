ef7e98a64c3a690901c6de38a265d5f9abbe4b9a
Scott <shur1@ualberta.ca>
2022-11-27 18:24:55 -0700

    Removed redundant code - Removed code for selection. This is already in IngredientSelectionViewAdapter

--- LOOPs/app/src/main/java/com/example/loops/adapters/IngredientStorageViewAdapter.java
+++ LOOPs/app/src/main/java/com/example/loops/adapters/IngredientStorageViewAdapter.java
@@ -29,26 +29,6 @@ import java.util.HashMap;
 public class IngredientStorageViewAdapter extends ArrayAdapter<Ingredient> {
     private Context context;
     private ArrayList<Ingredient> dataList;
-    private HashMap<String, ViewState> viewState;
-
-
-    /**
-     * https://stackoverflow.com/questions/6921462/listview-reusing-views-when-i-dont-want-it-to
-     * Date accessed: 2022-11-18
-     * Author: Xavi Gil
-     */
-    static class ViewHolder {
-        CardView cardView;
-    }
-
-    /**
-     * The various state each ingredient could be in
-     */
-    public enum ViewState {
-        Unselected,
-        Selected,
-        Pending
-    }
 
     /**
      * Adapter constructor.
@@ -59,7 +39,6 @@ public class IngredientStorageViewAdapter extends ArrayAdapter<Ingredient> {
         super(context, 0, dataList);
         this.context = context;
         this.dataList = dataList;
-        this.viewState = new HashMap<>();
     }
 
     @NonNull
@@ -67,20 +46,9 @@ public class IngredientStorageViewAdapter extends ArrayAdapter<Ingredient> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         Ingredient currentIngredient = dataList.get(position);
-        ViewHolder holder = null;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.ingredient_storage_row, parent, false);
-            holder = new ViewHolder();
-            holder.cardView = (CardView) convertView;
-            convertView.setTag(holder);
-
-            if (currentIngredient.getPending()) {
-                viewState.put(currentIngredient.getDocumentName(), ViewState.Pending);
-            }
-        }
-        else {
-            holder = (ViewHolder) convertView.getTag();
         }
 
         TextView description = convertView.findViewById(R.id.ingredient_description_in_storage);
@@ -132,47 +100,6 @@ public class IngredientStorageViewAdapter extends ArrayAdapter<Ingredient> {
 
         }
 
-        ViewState currentIngredientState = viewState.getOrDefault(currentIngredient.getDocumentName(), ViewState.Unselected);
-        //holder.cardView.setCardBackgroundColor(getBackgroundColorForState(currentIngredientState));
-
         return convertView;
-    }
-
-    /**
-     * Selects the item at the given position. Changes the background color of the associated view
-     * @param position
-     */
-    public void selectItem(int position) {
-        if (position >= dataList.size()) {
-            Log.e("DEBUG", "Tried to select item not in the ingredients data list");
-            return;
-        }
-        Ingredient selectedIngredient = dataList.get(position);
-        ViewState currentState = viewState.getOrDefault(selectedIngredient.getDocumentName(), ViewState.Unselected);
-        if (currentState == ViewState.Selected)
-            viewState.put(selectedIngredient.getDocumentName(), ViewState.Unselected);
-        else if (currentState == ViewState.Unselected)
-            viewState.put(selectedIngredient.getDocumentName(), ViewState.Selected);
-        notifyDataSetChanged();
-    }
-
-    /**
-     * Gets the background color id for given state
-     * @param state
-     * @return
-     */
-    private int getBackgroundColorForState(ViewState state) {
-        if (state == ViewState.Unselected) {
-            return context.getResources().getColor(R.color.dark_grey, null);
-        }
-        else if (state == ViewState.Selected) {
-            return Color.BLUE;
-        }
-        else if (state == ViewState.Pending) {
-            return Color.RED;
-        }
-        else {
-            throw new Error("No background color for the given state");
-        }
     }
 }
