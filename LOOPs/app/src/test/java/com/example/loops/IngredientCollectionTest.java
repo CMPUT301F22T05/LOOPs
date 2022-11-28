6af3add9064eaa2ebd51c51eec9eabec97cab6a5
Scott <shur1@ualberta.ca>
2022-11-27 21:34:08 -0700

    Updated test - Added few new tests - Refactored code a little

--- LOOPs/app/src/test/java/com/example/loops/IngredientCollectionTest.java
+++ LOOPs/app/src/test/java/com/example/loops/IngredientCollectionTest.java
@@ -52,19 +52,19 @@ public class IngredientCollectionTest {
                 1,
                 "",
                 "");
-        assertEquals(2, storage.getIngredients().size());
+        assertEquals(2, storage.size());
         storage.addIngredient(ingredient);
-        assertEquals(3, storage.getIngredients().size());
+        assertEquals(3, storage.size());
     }
 
     @Test
     void testDelete() {
         assertFalse(storage.deleteIngredient(-1));
-        assertEquals(2, storage.getIngredients().size());
+        assertEquals(2, storage.size());
         storage.deleteIngredient(1);
-        assertEquals(1, storage.getIngredients().size());
+        assertEquals(1, storage.size());
         storage.deleteIngredient(0);
-        assertEquals(0, storage.getIngredients().size());
+        assertEquals(0, storage.size());
     }
 
     @Test
@@ -77,48 +77,101 @@ public class IngredientCollectionTest {
                 "",
                 "");
         //assertFalse(storage.updateIngredient(3, ingredient));
-        assertEquals("a", storage.getIngredients().get(0).getDescription());
+        assertEquals("a", storage.get(0).getDescription());
         storage.updateIngredient(0, ingredient);
-        assertEquals("aa", storage.getIngredients().get(0).getDescription());
+        assertEquals("aa", storage.get(0).getDescription());
+    }
+
+    @Test
+    void testContains() {
+        assertTrue( storage.contains(ingredient1) );
+        assertTrue( storage.contains(ingredient2) );
+
+        Ingredient shouldNotContain = new Ingredient(
+        "xxx",
+        "2022-10-28",
+        "board",
+        5,
+        "",
+        "");
+        assertFalse( storage.contains(shouldNotContain) );
+    }
+
+    @Test
+    void testContainsSameDescriptionAndCategory() {
+        Ingredient shouldContain = new Ingredient(
+                "a",
+                "1965-10-11",
+                "diff",
+                5000,
+                "diff",
+                "x");
+        Ingredient shouldNotContain1 = new Ingredient(
+                "diff",
+                "1965-10-11",
+                "diff",
+                5000,
+                "diff",
+                "x");
+        Ingredient shouldNotContain2 = new Ingredient(
+                "a",
+                "1965-10-11",
+                "diff",
+                5000,
+                "diff",
+                "diff");
+
+        assertTrue( storage.containsDescriptionAndCategory(shouldContain) );
+        assertFalse( storage.containsDescriptionAndCategory(shouldNotContain1) );
+        assertFalse( storage.containsDescriptionAndCategory(shouldNotContain2) );
+    }
+
+    @Test
+    void testGetIngredient() {
+        Ingredient actualIng1 = storage.get(0);
+        Ingredient actualIng2 = storage.get(1);
+
+        assertEquals(ingredient1, actualIng1);
+        assertEquals(ingredient2, actualIng2);
     }
 
     @Test
     void testSortByDescription() {
         storage.sort(IngredientSortOption.BY_DESCRIPTION_DESCENDING);
-        assertEquals("b", storage.getIngredients().get(0).getDescription());
-        assertEquals("a", storage.getIngredients().get(1).getDescription());
+        assertEquals("b", storage.get(0).getDescription());
+        assertEquals("a", storage.get(1).getDescription());
         storage.sort(IngredientSortOption.BY_DESCRIPTION_ASCENDING);
-        assertEquals("a", storage.getIngredients().get(0).getDescription());
-        assertEquals("b", storage.getIngredients().get(1).getDescription());
+        assertEquals("a", storage.get(0).getDescription());
+        assertEquals("b", storage.get(1).getDescription());
     }
 
     @Test
     void testSortByBestBeforeDate() {
         storage.sort(IngredientSortOption.BY_BEST_BEFORE_DATE_DESCENDING);
-        assertEquals("2022-10-29", storage.getIngredients().get(0).getBestBeforeDateString());
-        assertEquals("2022-10-28", storage.getIngredients().get(1).getBestBeforeDateString());
+        assertEquals("2022-10-29", storage.get(0).getBestBeforeDateString());
+        assertEquals("2022-10-28", storage.get(1).getBestBeforeDateString());
         storage.sort(IngredientSortOption.BY_BEST_BEFORE_DATE_ASCENDING);
-        assertEquals("2022-10-28", storage.getIngredients().get(0).getBestBeforeDateString());
-        assertEquals("2022-10-29", storage.getIngredients().get(1).getBestBeforeDateString());
+        assertEquals("2022-10-28", storage.get(0).getBestBeforeDateString());
+        assertEquals("2022-10-29", storage.get(1).getBestBeforeDateString());
     }
 
     @Test
     void testSortByLocation() {
         storage.sort(IngredientSortOption.BY_LOCATION_DESCENDING);
-        assertEquals("fridge", storage.getIngredients().get(0).getStoreLocation());
-        assertEquals("cupboard", storage.getIngredients().get(1).getStoreLocation());
+        assertEquals("fridge", storage.get(0).getStoreLocation());
+        assertEquals("cupboard", storage.get(1).getStoreLocation());
         storage.sort(IngredientSortOption.BY_LOCATION_ASCENDING);
-        assertEquals("cupboard", storage.getIngredients().get(0).getStoreLocation());
-        assertEquals("fridge", storage.getIngredients().get(1).getStoreLocation());
+        assertEquals("cupboard", storage.get(0).getStoreLocation());
+        assertEquals("fridge", storage.get(1).getStoreLocation());
     }
 
     @Test
     void testSortByCategory() {
         storage.sort(IngredientSortOption.BY_CATEGORY_DESCENDING);
-        assertEquals("y", storage.getIngredients().get(0).getCategory());
-        assertEquals("x", storage.getIngredients().get(1).getCategory());
+        assertEquals("y", storage.get(0).getCategory());
+        assertEquals("x", storage.get(1).getCategory());
         storage.sort(IngredientSortOption.BY_CATEGORY_ASCENDING);
-        assertEquals("x", storage.getIngredients().get(0).getCategory());
-        assertEquals("y", storage.getIngredients().get(1).getCategory());
+        assertEquals("x", storage.get(0).getCategory());
+        assertEquals("y", storage.get(1).getCategory());
     }
 }
